1dc8d925db5920a71dd97b65ff09411014e03637
Abhishek <abhishek@example.com>
2026-01-07 11:48:24 +0530

    Updated backend URLs to deployed API

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Skills from "./Pages/Skills";
 import Navbar from "./components/Navbar";
@@ -13,8 +11,6 @@ const App = () => {
  <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
-    
-      <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/skills" element={<Skills />} />
     </Routes>
